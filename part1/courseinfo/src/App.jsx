ca3a5aaf64e14be9b767cf0e227457a250d41578
Nasir <nasirsabbir07@gmail.com>
2025-03-07 01:59:10 +0600

    add components to display the information

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.toc[0].name}: {props.toc[0].exercises} exercises
+      </p>
+      <p>
+        {props.toc[1].name}: {props.toc[1].exercises} exercises
+      </p>
+      <p>
+        {props.toc[2].name}: {props.toc[2].exercises} exercises
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.toc[0].exercises + props.toc[1].exercises + props.toc[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const toc = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header course={course}></Header>
+      <Content toc={toc}></Content>
+      <Total toc={toc}></Total>
+    </div>
+  );
+};
 
-export default App
+export default App;
